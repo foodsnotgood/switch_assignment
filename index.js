81ce1aaf5256f358f28cea82d7386fe7c0fac7b6
foodsnotgood <joroeder2588@gmail.com>
2022-04-15 15:47:23 +0200

    cleaning up

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ app.use(
 );
 app.options('*', cors());
 
-// // allow cross origin access
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-Requested-With,content-type'
-//   );
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
-
 app.use(express.json());
 
 app.listen(process.env.PORT || 3000, () => {
